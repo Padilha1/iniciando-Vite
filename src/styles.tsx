0f11ac7e37dcb1157d34b17118105976dce88ec8
Padilha1 <padilha.matheus@hotmail.com>
2022-03-24 23:04:38 -0300

    Firsts steps with Nav

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,4 +1,10 @@
 import styled, { createGlobalStyle } from "styled-components";
+import "./css/fonts.css"
+
+// color:#845EC2;
+// color:#4B4453
+// color:#F3C5FF;
+// color:#B0A8B9;
 
 
 export const GlobalStyle = createGlobalStyle`
@@ -9,20 +15,27 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing:border-box;
         margin: 0;
         padding:0;
+        font-family:'Open Sans';
+        font-weight:normal;
+        background:#4B4453;
+        width:100%;
+        height:100%;
     }
+    a{
+        color: #B0A8B9;
+        text-decoration:none;
+    }
+
 `;
 
-export const Title = styled.h2`
+export const Title = styled.h1`
     font-size: 1.2 em;
 `;
 
 export const NavContent = styled.div`
-    border-box
-    border: 1px solid black;
     background: transparent;
-    padding: 1em;   
-    margin: 0;
-    width: 100%;
+    width: 100;
+    padding-top:1em;
 `;
 export const NAV = styled.div `
     display: flex;
@@ -30,3 +43,9 @@ export const NAV = styled.div `
     align-items: center;
     
 `;
+
+export const SpaceDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap:1em;
+`
